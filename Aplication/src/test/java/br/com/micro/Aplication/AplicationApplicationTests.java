ef5b7fd9e78adf798bf01135fa83e94fa6a56a6e
= <jcarlos.alegria2015@gmail.com>
2025-05-01 15:35:16 -0300

    projeto

---
+++ Aplication/src/test/java/br/com/micro/Aplication/AplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.micro.Aplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
